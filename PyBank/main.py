5c0729f5b9629ba84b9d3925dafbd4bce9a85f97
jacob-servidio <67562203+jacob-servidio@users.noreply.github.com>
2020-08-10 17:57:04 -0400

    adding output to txtfile

--- PyBank/main.py
+++ PyBank/main.py
@@ -69,3 +69,16 @@ print(f"Total: ${net_amount}")
 print(f"Average Change: {average_change:.2f}")
 print(f"Greatest Inc in Profits:, {greatest_increase_month}, ( ${highest})")
 print(f"Greatest Dec in Profits:, {greatest_decrease_month}, (${lowest})")
+
+#file to write analysis to
+output_file = os.path.join('..', 'Pybank', 'analysis', 'analysis_results.txt')
+
+with open(output_file, 'w',) as txtfile:
+
+    txtfile.write(f"Financial Analysis\n")
+    txtfile.write(f"--------------------------\n")
+    txtfile.write(f"Total Months: {total_months}\n")
+    txtfile.write(f"Total: ${net_amount}\n")
+    txtfile.write(f"Average Change: {average_change:.2f}\n")
+    txtfile.write(f"Greatest Inc in Profits:, {greatest_increase_month}, ( ${highest})\n")
+    txtfile.write(f"Greatest Dec in Profits:, {greatest_decrease_month}, (${lowest})\n")
